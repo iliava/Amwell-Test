cb14de757b0841c83818b46b75e49a2cc2bc6205
iliava <85126509+iliava@users.noreply.github.com>
2022-01-06 00:42:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ def url_check():
         if (requests.get("http://10.2.3.233").status_code) == 200:
             return render_template('Good.html')
         else:
-            return render_template('bad.html')
+            return render_template('Bad.html')
     except:
-        return render_template('bad.html')
+        return render_template('Bad.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
